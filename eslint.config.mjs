75a20372ce3faa02e600cf7020b097b5aead18b4
Go3105 <sato.go@ohsuga.lab.uec.ac.jp>
2025-03-11 22:58:23 +0900

    chapter14まで

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,7 +15,7 @@ export default [
             "react/react-in-jsx-scope": "off",
             "react/jsx-uses-react": "off",
             "no-unused-vars": "off",
-            "@typescript-eslint/no-unused-vars": ["error"]
+            "@typescript-eslint/no-unused-vars": "off"
         },
     },
 ];
