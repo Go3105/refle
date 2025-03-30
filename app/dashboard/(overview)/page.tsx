729bc9d3e000cab98ae78a161230b2848187a00d
Go3105 <sato.go@ohsuga.lab.uec.ac.jp>
2025-03-31 01:57:19 +0900

    不要なフォルダ・ファイルを削除。トーク開始画面を試しに作ってみた

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,31 +1,23 @@
-import CardWrapper from '@/app/ui/dashboard/cards';
-import RevenueChart from '@/app/ui/dashboard/revenue-chart';
-import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
-import { lusitana } from '@/app/ui/fonts';
-import { Suspense } from 'react';
-import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
+import Image from 'next/image';
+import Link from 'next/link';
 
-export default async function Page() {
-
-
+export default function Page() {
     return (
-        <main>
-            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-                Dashboard
-            </h1>
-            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-                <Suspense fallback={<CardsSkeleton />}>
-                    <CardWrapper />
-                </Suspense>
+        <main className="flex justify-center items-center h-screen">
+            <div className="text-center">
+                <Image
+                    src="/ai_character.png"
+                    width={200}
+                    height={200}
+                    alt="AI character"
+                />
+                <Link
+                    href="dashboard/conversation"
+                    className="mt-[25vh] block rounded-lg bg-pink-100 px-5 py-5 font-serif hover:bg-pink-200"
+                >
+                    <p>今日の振り返りを始める</p>
+                </Link>
             </div>
-            <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-                <Suspense fallback={<RevenueChartSkeleton />}>
-                    <RevenueChart />
-                </Suspense>
-                <Suspense fallback={<LatestInvoicesSkeleton />}>
-                    <LatestInvoices />
-                </Suspense>
-            </div>
-        </main>
+        </main >
     );
 }
---
+++ app/dashboard/summary/page.tsx
@@ -0,0 +1,13 @@
+import { lusitana } from '../../ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Invoices',
+};
+
+export default async function Page() {
+
+    return (
+        <div></div>
+    );
+}
