ec273182b0a1b4672982cce3a66e6ce3dd015dcf
Go3105 <sato.go@ohsuga.lab.uec.ac.jp>
2025-04-26 16:09:59 +0900

    会話開始画面のUI変更

---
+++ app/components/ui/Microphoneicon.tsx
@@ -0,0 +1,21 @@
+import MicrophoneAnimation from '../../../public/MicrophoneAnimation.json';
+import dynamic from 'next/dynamic';
+
+const Lottie = dynamic(() => import('lottie-react'), { ssr: false });
+
+const Microphoneicon = () => {
+    return (
+        <Lottie
+            animationData={MicrophoneAnimation}
+            loop={true}
+            autoplay={true}
+            className="mb-8"
+            style={{
+                width: '100%',
+                height: '100%',
+            }}
+        />
+    );
+}
+
+export default Microphoneicon;
